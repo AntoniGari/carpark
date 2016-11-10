20804f77cb59dc29800d38a5a76981a86f27cd58
AntoniGari <slemonboy@hotmail.com>
2016-11-10 20:27:53 +0100

    First Car Test
    With suspension and acceleration

---
+++ Assets/Scripts/Player/CarMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class CarMovement : MonoBehaviour {
+
+	public float wheelRadius;
+
+	public Transform centerOfMass;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.centerOfMass = centerOfMass.localPosition;
+	}
+
+	void FixedUpdate () {
+		var inputDevice = InputManager.ActiveDevice;
+
+		Debug.Log (Input.GetAxisRaw ("Right Trigger"));
+		rb.AddForce (1000 * Vector3.forward * Input.GetAxisRaw("Right Trigger"));
+	}
+}
