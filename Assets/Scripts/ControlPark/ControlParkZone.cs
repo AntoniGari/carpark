c49f7fd0cac32015267239343da063564780bc3d
AntoniGari <slemonboy@hotmail.com>
2017-01-31 18:33:04 +0100

    Creating the Park Area
    -Parking Controller
    -Defining Parking Area

---
+++ Assets/Scripts/ControlPark/ControlParkZone.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlParkZone : MonoBehaviour {
+
+	int wheels;
+
+	private IEnumerator parkTimer;
+
+	// Use this for initialization
+	void Start () {
+		wheels = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.CompareTag("Wheel")) {
+			++wheels;
+			//Debug.Log ("Wheel Enter: " + wheels);
+			if (wheels == 4)
+				Debug.Log ("Comença Coroutina");
+				parkTimer = ControlParkTime (5.0f); 
+				StartCoroutine (parkTimer);
+		}
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.gameObject.CompareTag("Wheel")) {
+			//Debug.Log ("Wheel Exit: " + wheels);
+			if (wheels == 4) {
+				Debug.Log ("Para Coroutina");
+				StopCoroutine (parkTimer);
+			}
+			--wheels;
+		}
+	}
+
+	private IEnumerator ControlParkTime(float waitTime) {
+		yield return new WaitForSeconds (waitTime);
+		Debug.Log ("Aparcat!");
+	}
+}
---
+++ Assets/Scripts/ControlPark/DefineParkZone.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefineParkZone : MonoBehaviour {
+
+	/// <summary>
+	/// The color of the line.
+	/// </summary>
+	public Color lineColor;
+
+	/// <summary>
+	/// The nodes.
+	/// </summary>
+	private List<Transform> nodes = new List<Transform>();
+
+	/// <summary>
+	/// Raises the draw gizmos selected event.
+	/// </summary>
+	void OnDrawGizmos() {
+		Gizmos.color = lineColor;
+
+		Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+		nodes = new List<Transform>();
+
+		for(int i = 0; i < pathTransforms.Length; i++) {
+			if(pathTransforms[i] != transform) {
+				nodes.Add(pathTransforms[i]);
+			}
+		}
+
+		for(int i = 0; i < nodes.Count; i++) {
+			Vector3 currentNode = nodes[i].position;
+			Vector3 previousNode = Vector3.zero;
+
+			if (i > 0) {
+				previousNode = nodes[i - 1].position;
+			} else if(i == 0 && nodes.Count > 1) {
+				previousNode = nodes[nodes.Count - 1].position;
+			}
+
+			Gizmos.DrawLine(previousNode, currentNode);
+			Gizmos.DrawWireSphere(currentNode, 0.3f);
+		}
+	}
+}
