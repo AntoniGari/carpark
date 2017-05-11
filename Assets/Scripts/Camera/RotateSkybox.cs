981825049c35bc1ea263405979840574855afee9
AntoniGari <slemonboy@hotmail.com>
2017-05-11 17:40:19 +0200

    ICade Tests in Sandbox
    Doing tests with the Car Controller

---
+++ Assets/Scripts/Camera/RotateSkybox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour {
+
+	/// <summary>
+	/// The skybox's scroll speed.
+	/// </summary>
+	private float scrollSpeed;
+
+	/// <summary>
+	/// The horizontal offset.
+	/// </summary>
+	private float horizontalOffset;
+
+	// Use this for initialization
+	void Start () {
+		scrollSpeed = 2.0f;
+		horizontalOffset = 0.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Set Horizontal Offset
+		horizontalOffset = horizontalOffset + (scrollSpeed * Time.deltaTime);
+
+		//Animate Texture.
+		RenderSettings.skybox.SetFloat ("_Rotation", horizontalOffset);
+	}
+}
---
+++ Assets/Scripts/Player/Car/CarUserControl.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using InControl;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+
+			#if UNITY_IOS
+			ICadeDeviceManager.Active = true;
+			#endif
+        }
+
+
+        private void FixedUpdate()
+        {
+
+			var inputDevice = InputManager.ActiveDevice;
+
+			// pass the input to the car!
+			#if UNITY_IOS
+				float h = inputDevice.LeftStickX;
+				float v = inputDevice.LeftStickX;
+			#else
+				float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			Debug.Log ("H: " + h);
+				float v = CrossPlatformInputManager.GetAxis("Vertical");
+			#endif
+
+
+			#if !MOBILE_INPUT
+			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+			m_Car.Move(h, v, v, handbrake);
+			#else
+			m_Car.Move(h, v, v, 0f);
+			#endif
+
+
+        }
+    }
+}
