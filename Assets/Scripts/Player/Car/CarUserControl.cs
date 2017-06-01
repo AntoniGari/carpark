bcbc2b73961f549e9295e967bdd84c517d3252de
AntoniGari <slemonboy@hotmail.com>
2017-06-01 17:15:58 +0200

    ICade Tests in Sandbox
    Doing Tests with the Car Controller

--- Assets/Scripts/Player/Car/CarUserControl.cs
+++ Assets/Scripts/Player/Car/CarUserControl.cs
@@ -10,19 +10,16 @@ namespace UnityStandardAssets.Vehicles.Car
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-		private ControllerHelper test;
 
 		public Text coordenadesX;
 		public Text coordenadesY;
 
-		//NEW
 		TwoAxisInputControl filteredDirection;
 
         private void Awake()
         {
             // get the car controller
             m_Car = GetComponent<CarController>();
-			//test = GetComponent<ControllerHelper> ();
 
 			#if UNITY_IOS
 			ICadeDeviceManager.Active = true;
@@ -36,23 +33,13 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void FixedUpdate()
         {
-			//GameManager.Instance.controllerManager.
+			float h = 0.0f;
+			float v = 0.0f;
 
 
-			float h;
-			float v;
 
 			// pass the input to the car!
 			#if UNITY_IOS
-				/*var inputDevice = InputManager.ActiveDevice;
-				var controlX = inputDevice.LeftStickX;
-				float h = controlX.Value;
-				var controlY = inputDevice.LeftStickY;
-				float v = controlY.Value;
-				//coordenadesX.text = string.Format( "{0} {1}", "Left Stick X = ", test.GetLeftStickX());
-				//coordenadesY.text = string.Format( "{0} {1}", "Left Stick Y = ", test.GetLeftStickY());
-				m_Car.Move(h, v, v, 0f);
-				*/
 			var inputDevice = InputManager.ActiveDevice;
 			filteredDirection.Filter (inputDevice.Direction, Time.deltaTime);
 
@@ -61,6 +48,9 @@ namespace UnityStandardAssets.Vehicles.Car
 			} else if (filteredDirection.Down.WasPressed) {
 			v = -1.0f;
 			}
+
+			coordenadesY.text = v.ToString ();
+			coordenadesX.text = h.ToString ();
 			#else
 			h = CrossPlatformInputManager.GetAxis("Horizontal");
 			v = CrossPlatformInputManager.GetAxis("Vertical");
@@ -70,8 +60,10 @@ namespace UnityStandardAssets.Vehicles.Car
 			#if !MOBILE_INPUT
 			float handbrake = CrossPlatformInputManager.GetAxis("Jump");
 			m_Car.Move(h, v, v, handbrake);
+			Debug.Log("H: " + h + " - V: " + v);
+
 			#else
-			m_Car.Move(h, v, v, 0f);
+			m_Car.Move(h, v, v, 1f);
 			#endif
 
 
